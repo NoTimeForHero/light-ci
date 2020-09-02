e40ed8a498efa7306cc0a67ceb8a697370e65d62
NoTimeForHero <notimeforhero@yandex.ru>
2020-09-03 02:38:16 +0300

    Подправил JWT пример

--- example/jwt-auth/configs/light-ci/config.js
+++ example/jwt-auth/configs/light-ci/config.js
@@ -1,28 +1,16 @@
-const projects = {
-    'test-project1': {
-      //script: 'build.sh', // Возможность поменять название сборочного скрипта для конкретного проекта
-      security: {
-        type: 'GITHUB',
-        secret: 'BLj1O-G_vwAAIKZRthEN8'
-      }
-    }
-  };
-
-  const database = {
-    type: 'sqlite', // Допустимые значения: ['memory', 'sqlite']
-    filename: '/app/temp/sqlite.dat'
-  };
-
-  // const authorization = null;
+const database = {
+  type: 'sqlite',
+  filename: '/app/temp/sqlite.dat'
+};
+ 
+const authorization = {
+  type: 'jwt',
+  secret: process.env.SECRET_KEY,
+  authUrl: process.env.AUTH_URL,
+  alghorithm: 'HS256'
+};
   
-  const authorization = {
-    type: 'jwt',
-    secret: process.env.SECRET_KEY ?? 'your_secret_key',
-    authUrl: process.env.AUTH_URL ?? 'http://localhost:4000/app/login?callback=',
-    alghorithm: 'HS256'
-  };
-  
-  export default {
-    projects, authorization, database
-  };
+export default {
+  projects, authorization, database
+};
   
--- example/jwt-auth/configs/light-ci/entrypoint.sh
+++ example/jwt-auth/configs/light-ci/entrypoint.sh
@@ -1,2 +1,1 @@
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-ssh-keyscan -H example.org >> ~/.ssh/known_hosts
